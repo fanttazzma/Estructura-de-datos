242ee97104a095266192d4135c578fdf4e9f8531
fanttazzma 666 <fanttazma@outlook.com>
2025-01-30 21:50:12 -0600

    cambio.001

--- Estructura  de datos/Estructura  de datos.cpp
+++ Estructura  de datos/Estructura  de datos.cpp
@@ -1,19 +1,23 @@
-// Estructura  de datos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
+#include <iostream> 
+using namespace std;
+const int num = 8;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Hello world!\n";
+	int nums[num];
+	int total = 0;
+	for (int i = 0; i < num; i++)
+	{
+		cout << "Introduce el numero: ";
+		cin >> nums[i];
+
+		total += nums[i];
+
+
+	}
+	cout << "El total de numero es: " << total << endl;
+	system("pause");
+
+	return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
